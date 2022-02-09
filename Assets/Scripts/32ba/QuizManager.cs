2adfa2f41006bb5e2eeba52940210bc22c72fcf0
32ba <dev@32ba.net>
2022-02-09 19:52:07 +0900

    クイズの正解/不正解をわかりやすくした

--- Assets/Scripts/32ba/QuizManager.cs
+++ Assets/Scripts/32ba/QuizManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,33 +12,84 @@ public class QuizManager : MonoBehaviour
     public GameObject choicesB;
     public GameObject choicesC;
     public GameObject choicesD;
+    public GameObject correctTextObject;
+    public GameObject incorrectTextObject;
     public Text questionText;
-    
-    private List<string[]> _questionData = new List<string[]>();
-    private bool _isQuestionCsvLoaded = false;
+
+    private readonly List<string[]> _questionData = new List<string[]>();
     private int _questionId;
-    
+    private bool _isAlreadyAnswered = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        _isQuestionCsvLoaded = CsvReader.Read(questionCsv, _questionData);
-        Debug.Log(_isQuestionCsvLoaded ? "問題CSVの読み込みに成功しました" : "問題CSVの読み込みに失敗しました");
+        bool isQuestionCsvLoaded = CsvReader.Read(questionCsv, _questionData);
+        Debug.Log(isQuestionCsvLoaded ? "問題CSVの読み込みに成功しました" : "問題CSVの読み込みに失敗しました");
         _questionId = UnityEngine.Random.Range(0, _questionData.Count);
         SetQuestion(_questionData, _questionId);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnClick(string answer)
     {
-        
+        if (_isAlreadyAnswered) return;
+        _isAlreadyAnswered = true;
+        if (AnswerQuestion(_questionData, _questionId, answer))
+        {
+            correctTextObject.SetActive(true);
+            HighlightCorrectAnswer(_questionData, _questionId);
+            //ToDo:正解したらポイントを付与する
+        }
+        else
+        {
+            //ToDo:不正解なら正しい答えをハイライトして次へすすむ
+            incorrectTextObject.SetActive(true);
+            HighlightCorrectAnswer(_questionData, _questionId);
+        }
+
     }
 
-    void SetQuestion(List<string[]> data, int id)
+    private void SetQuestion(List<string[]> data, int id)
     {
         questionText.text = _questionData[id][0];
-        choicesA.GetComponentInChildren<Text>().text = _questionData[id][1];
-        choicesB.GetComponentInChildren<Text>().text = _questionData[id][2];
-        choicesC.GetComponentInChildren<Text>().text = _questionData[id][3];
-        choicesD.GetComponentInChildren<Text>().text = _questionData[id][4];
+        choicesA.GetComponentInChildren<Text>().text = data[id][1];
+        choicesB.GetComponentInChildren<Text>().text = data[id][2];
+        choicesC.GetComponentInChildren<Text>().text = data[id][3];
+        choicesD.GetComponentInChildren<Text>().text = data[id][4];
+    }
+    
+    private bool AnswerQuestion(List<string[]> data, int id, string answer)
+    {
+        var isCorrect = (data[id][5] == answer);
+        Debug.Log(isCorrect ? "正解" : "不正解");
+        return isCorrect;
+    }
+
+    private void HighlightCorrectAnswer(List<string[]> data, int id)
+    {
+        var choicesAButton = choicesA.GetComponent<Button>();
+        var choicesBButton = choicesB.GetComponent<Button>();
+        var choicesCButton = choicesC.GetComponent<Button>();
+        var choicesDButton = choicesD.GetComponent<Button>();
+        choicesAButton.interactable = false;
+        choicesBButton.interactable = false;
+        choicesCButton.interactable = false;
+        choicesDButton.interactable = false;
+        
+        var correctAnswer = data[id][5];
+        switch (correctAnswer)
+        {
+            case "A":
+                choicesAButton.interactable = true;
+                break;
+            case "B":
+                choicesBButton.interactable = true;
+                break;
+            case "C":
+                choicesBButton.interactable = true;
+                break;
+            case "D":
+                choicesBButton.interactable = true;
+                break;
+        }
     }
 }
