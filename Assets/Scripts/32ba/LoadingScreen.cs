e24aab12814c4722310542e186ebf8cab8a7a1e6
32ba <dev@32ba.net>
2022-02-10 02:04:58 +0900

    ローディング画面の実装
    オーバーレイなのでお好きな場面で出せます

---
+++ Assets/Scripts/32ba/LoadingScreen.cs
@@ -0,0 +1,30 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+
+
+public class LoadingScreen : MonoBehaviour
+{
+    public Text loadingText;
+    private string _text = "Loading";
+    private int _step = 0;
+    
+    private void OnEnable()
+    {
+        Observable.Interval(TimeSpan.FromSeconds(0.4f)).TakeUntilDisable(this).Subscribe(_ => TextController());
+        //ToDo:横にサンマが跳ねてるLive2Dを入れたら見栄えが良さそう
+    }
+
+    private void TextController()
+    {
+        _text += ".";
+        _step++;
+        if (_step == 4)
+        {
+            _step = 0;
+            _text = "Loading";
+        }
+        loadingText.text = _text;
+    }
+}
