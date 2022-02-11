9f6fde08568b988f1e8ecd2c8c6a531efe57a8a1
fuwamocha <74049905+fuwamocha@users.noreply.github.com>
2022-02-11 22:17:35 +0900

    セリフの取得方法を変更・現時点までで集まったセリフを追加

--- Assets/Scripts/nemui/Home/DialogueController.cs
+++ Assets/Scripts/nemui/Home/DialogueController.cs
@@ -4,10 +4,16 @@ using UniRx;
 public class DialogueController : MonoBehaviour
 {
   [SerializeField] private Button screenTouchArea = default;
-  [SerializeField] private string[] characterDialogue = default;
+  [SerializeField] private TextAsset dialogueTextAsset = default;
+
+  private string[] dialogueDataArray;
+  private string dialogueData;
   private Text dialogueText;
   void Start()
   {
+    dialogueData = dialogueTextAsset.text;
+    dialogueDataArray = dialogueData.Split('\n');
+
     dialogueText = this.gameObject.transform.GetChild(0).GetComponent<Text>();
 
     screenTouchArea = screenTouchArea.GetComponent<Button>();
@@ -20,8 +26,12 @@ public class DialogueController : MonoBehaviour
     OnClickScreenTouchArea(); // はじめに一回だけ実行する
   }
 
+
+  /// <summary>
+  /// 画面をクリックしたときにランダムでセリフを吐くクラス
+  /// </summary>
   private void OnClickScreenTouchArea()
   {
-    dialogueText.text = characterDialogue[Random.Range(0, characterDialogue.Length)];
+    dialogueText.text = dialogueDataArray[Random.Range(0, dialogueDataArray.Length)];
   }
 }
