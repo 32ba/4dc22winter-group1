ebeb8ecd25417b838c33b94c942f9ec0f78fa8f6
pekepekee <takkummm78@gmail.com>
2022-02-11 12:27:05 +0900

    プロローグのシナリオを少し変更

--- Assets/Scripts/pekepeke/TextWriterTuterial.cs
+++ Assets/Scripts/pekepeke/TextWriterTuterial.cs
@@ -37,21 +37,21 @@ public class TextWriterTuterial : MonoBehaviour
         uitext.DrawText("夏の暑さが和らぎ、木の葉の青々とした山はだんたんと色鮮やかになった。");
         yield return StartCoroutine("Skip");
 
-        uitext.DrawText("その山を背景に、人はそれほど多くはないが、少しの平たい土地を奪い合うかのように乱立する建物がせめぎあう宿場町があった。");
+        uitext.DrawText("その山を背景に、人が老人と子供しかおらず若い者は出稼ぎに行く貧しい田舎村があった。");
         yield return StartCoroutine("Skip");
 
 
-        uitext.DrawText("依頼主", "実はですねぇ、今年の夏頃に町の北の方に新しく魚屋ができたんですが・・・");
+        uitext.DrawText("依頼主", "実はですねぇ、今年の夏頃に村の北の方に新しく魚屋ができたんですが・・・");
         yield return StartCoroutine("Skip");
 
         change.ChangeSad();
         uitext.DrawText("探偵", "・・・");
         yield return StartCoroutine("Skip");
 
-        uitext.DrawText("宿場の一室で依頼主と探偵は、机を挟み淹れたての緑茶を飲みながら会話をしている。");
+        uitext.DrawText("村の集会所の隅で依頼主と探偵は、机を挟み淹れたての緑茶を飲みながら会話をしている。");
         yield return StartCoroutine("Skip");
 
-        uitext.DrawText("依頼主", "住人の話しによると、その店では新鮮な海の魚も売っているとのことなんですが、どうにも抽選機を回して出た色で買える魚が変わるという話で。");
+        uitext.DrawText("依頼主", "村人の話しによると、その店では新鮮な海の魚を売っているとのことなんですが、どうにも抽選機を回して出た色で買える魚が変わるという話で。");
         yield return StartCoroutine("Skip");
 
         change.ChangeSmile();
@@ -64,28 +64,28 @@ public class TextWriterTuterial : MonoBehaviour
         uitext.DrawText("探偵", "確かに変な売り方ですね。とても儲けが出る売り方とは思えないです。");
         yield return StartCoroutine("Skip");
 
-        uitext.DrawText("依頼主", "私もそう思うのですが、何回抽選しても新鮮な海の魚が出てきたところは見たことがないと住人から陳情が来ていて、本当に海の魚を売っているか怪しいのです。");
+        uitext.DrawText("依頼主", "私もそう思うのですが、何回抽選してもマグロが出てきたところは見たことがないと村人から陳情が来ていて、本当にマグロを売っているか怪しいのです。");
         yield return StartCoroutine("Skip");
 
-        uitext.DrawText("依頼主", "そこで探偵さんには、本当に新鮮な海の魚を仕入れているのかそのお店を調査してもらいたくて・・・");
+        uitext.DrawText("依頼主", "そこで探偵さんには、本当にマグロを仕入れているのかそのお店を調査してもらいたくて・・・");
         yield return StartCoroutine("Skip");
 
         change.ChangeSad();
         uitext.DrawText("探偵", "・・・");
         yield return StartCoroutine("Skip");
 
-        uitext.DrawText("部屋に一瞬の静寂が訪れる。探偵は少し考える素振りをし、口を開いた。");
+        uitext.DrawText("集会所の隅に一瞬の静寂が訪れる。探偵は少し考える素振りをし、口を開いた。");
         yield return StartCoroutine("Skip");
 
         change.ChangeSmile();
         uitext.DrawText("探偵", "分かりました。しかし、かなりの費用はかかると思います。");
         yield return StartCoroutine("Skip");
 
-        uitext.DrawText("この宿場町は海からかなり離れており、新鮮な魚を運んでくるには相応の費用が掛かることは想像がつく。");
+        uitext.DrawText("この村は海からかなり離れており、新鮮な魚を運んでくるには相応の費用が掛かることは想像がつく。");
         yield return StartCoroutine("Skip");
 
         change.ChangeSad();
-        uitext.DrawText("しかし、話題に挙がった魚屋で本当に海の魚を仕入れているのかは分からない。");
+        uitext.DrawText("しかし、話題に挙がった魚屋で本当にマグロ仕入れているのかは分からない。");
         yield return StartCoroutine("Skip");
 
         uitext.DrawText("探偵はどことなく深い闇を感じた。");
@@ -95,5 +95,5 @@ public class TextWriterTuterial : MonoBehaviour
         yield return StartCoroutine("Skip");
         yield return StartCoroutine("TutorialGacha");
     }
-    // Update is called once per frame
+    
 }
