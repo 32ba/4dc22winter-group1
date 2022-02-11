94f43cd623cc2d55b41bc144241c940cba59022d
pekepekee <takkummm78@gmail.com>
2022-02-11 12:12:57 +0900

    サンマifシナリオ追加
    チュートリアルガチャでサンマしか出なかった用のシナリオを追加

---
+++ Assets/Scripts/pekepeke/TextWriterSanmaIf.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TextWriterSanmaIf : MonoBehaviour
+{
+    public Uitext uitext;
+    public ImageChange change;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("Cotest");
+    }
+
+    IEnumerator Skip()
+    {
+        while (uitext.playing) yield return 0;
+        while (!uitext.IsClicked()) yield return 0;
+    }
+
+    IEnumerator Home()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene("Home");
+        }
+        yield return 0;
+    }
+    // 文章を表示させるコルーチン
+    IEnumerator Cotest()
+    {
+        uitext.DrawText("依頼主", "サンマしか当たらなかったですね・・・");
+        yield return StartCoroutine("Skip");
+
+        uitext.DrawText("探偵", "まあまだ10回しか引いてないからねぇ。");
+        yield return StartCoroutine("Skip");
+
+        uitext.DrawText("依頼主", "そうですね。気長にガチャを回し続けますか。");
+        yield return StartCoroutine("Skip");
+
+        uitext.DrawText("探偵", "ええ。そうしましょう。");
+        yield return StartCoroutine("Skip");
+
+        yield return StartCoroutine("Home");
+    }
+
+}
