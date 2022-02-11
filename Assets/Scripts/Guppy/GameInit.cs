8ecb3dac2d67b7d9b2f6d2b878c4e3810f370582
Guppy-ZEN <39458650+g-zen@users.noreply.github.com>
2022-02-11 22:04:30 +0900

    ガチャの設定を調整

--- Assets/Scripts/Guppy/GameInit.cs
+++ Assets/Scripts/Guppy/GameInit.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class GameInit : MonoBehaviour
 {
-    private const int initialPoint = 1000;
+    private const int initialPoint = 10000;
     // Start is called before the first frame update
     void Start()
     {
