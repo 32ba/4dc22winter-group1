dbabd0d7776b35e95be5f78145c4169473dc901e
32ba <dev@32ba.net>
2022-02-12 12:26:41 +0900

    クイズシーン BGMの追加

---
+++ Assets/Scripts/32ba/QuizBGMManager.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class QuizBGMManager : MonoBehaviour
+{
+    private static QuizBGMManager Instance {
+        get;
+        set;
+    }
+
+    private void Awake(){
+        if (Instance != null) {
+            Destroy (gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad (gameObject);
+    }
+
+    private void Start()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnSceneLoaded(Scene next, LoadSceneMode mode)
+    {
+        if(next.name == "quiz") return;
+        Destroy(gameObject);
+    }
+
+}
